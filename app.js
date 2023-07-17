fd2f5e761f71e3143263d362fb75853eb9c66948
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2023-07-18 01:38:07 +0530

    seo 1 seo-sitemap-generator

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const nocache = require("nocache");
 const session = require("express-session");
-const router = express.Router();
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
@@ -15,6 +14,7 @@ const Category = require("./models/category");
 const bodyParser = require("body-parser");
 const cron = require("node-cron");
 const User = require("./models/users");
+const { generateSitemap } = require("seo-sitemap-generator");
 const {
   clearCartAndMoveToWishlist,
 } = require("./controller/clearCartController");
@@ -116,6 +116,22 @@ app.use(async (req, res, next) => {
   } else {
     next();
   }
+});
+const urls = [
+  "https://soundshoppie.de/",
+  "https://soundshoppie.de/signupMobile",
+  "https://soundshoppie.de/signup",
+  "https://soundshoppie.de/login",
+  "https://soundshoppie.de/landingPage",
+  "https://soundshoppie.de/viewProducts",
+  // Add more URLs as needed
+];
+
+// Route or middleware for serving the sitemap
+app.get("/sitemap.xml", (req, res) => {
+  const sitemap = generateSitemap(urls);
+  res.header("Content-Type", "application/xml");
+  res.send(sitemap);
 });
 
 app.listen(PORT, () => {
